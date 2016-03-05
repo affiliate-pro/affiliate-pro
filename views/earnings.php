2e72a75e3aaf304785e59ed4cb54dc35d6555667
Rasmus Kjellberg <rk@youngmedia.se>
2016-03-05 21:01:00 +0100

    Translated earnings

--- views/earnings.php
+++ views/earnings.php
@@ -1,10 +1,18 @@
 <div id="ymas" class="wrap">
-	<h1>Affiliate <small>&gt; Earnings</small></h1>
-	<?php 	
-	if ($ymas->adtraction->isConfigured())
-		require_once('earnings-adtraction.php');
-
-	if ($ymas->adrecord->isConfigured())
-		require_once('earnings-adrecord.php');
-	?> 
+	<h1><?php _e('Affiliate', 'ymas');?> <small>&gt; <?php _e('Earnings', 'ymas');?></small></h1>
+	<h2><?php _e('Latest transactions', 'ymas');?></h2>
+	<table class="wp-list-table widefat fixed striped" cellspacing="0">
+	<thead>
+		<tr>
+			<th class="lang-column"></th>
+			<th class="logo-column"><?php _e('Program', 'ymas');?></th>
+			<th class="category-column"><?php _e('Category', 'ymas');?></th>
+			<!--th>Program</th -->
+			<th class="compensation-column"><?php _e('Compensations', 'ymas');?></th>
+			<th class="url-column"><?php _e('Tracking URL', 'ymas');?></th>
+		</tr>
+	</thead>
+	<tbody>
+	</tbody>
+	</table>
 </div>
